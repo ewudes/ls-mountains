da823a027f71be0c2a479f589e318847ce281ca6
alekseyGunchin <a.s.gunchin@gmail.com>
2018-06-18 22:37:23 +0300

    finished the job the second week

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,7 +1,9 @@
 module.exports = {
   white: "#efeee9",
   beige: "#eeebdf",
+	"light-beige": "#efebe0",
   green: "#00bea4",
+	"dark-green": "#268c8b",
   "text-color": "#455a64",
   grey: "#373e42",
   "light-grey": "#dfdcd5"
